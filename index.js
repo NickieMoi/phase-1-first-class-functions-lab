485ef8a7558077e565325f9e22495b77f8675e6e
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-08 23:16:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= function(drivers){
+    return (["Antonia","Nuru"])
+}
+const returnLastTwoDrivers= function(drivers){
+    return(['Amari', 'Mo' ])
+}
+const selectingDrivers =[returnFirstTwoDrivers ,returnLastTwoDrivers];
+const createFareMultiplier= function(multiply){
+    return function(multiply) {
+        return multiply*5;
+    }
+}
+const fareDoubler= function (createFareMultiplier){
+    return (5*4)
+}
+const fareTripler= function(createFareMultiplier){
+    return createFareMultiplier*3
+}
+const selectDifferentDrivers= function(drivers,driversToReturn){
+    return driversToReturn(drivers);
+    
+}
